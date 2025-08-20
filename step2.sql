7a501fe56b148fb56980bf5493d68ce0639fe4cd
uttam206 <153041433+uttam206@users.noreply.github.com>
2025-08-20 21:57:41 +0530

    Create step2.sql

---
+++ step2.sql
@@ -0,0 +1,14 @@
+-- Employees data
+INSERT INTO Employees (emp_id, emp_name, dept_id) VALUES
+(1, 'Amit', 101),
+(2, 'Neha', 102),
+(3, 'Rohit', 103),
+(4, 'Priya', NULL),
+(5, 'Vikas', 105);
+
+-- Departments data
+INSERT INTO Departments (dept_id, dept_name) VALUES
+(101, 'HR'),
+(102, 'Finance'),
+(103, 'IT'),
+(104, 'Marketing');
